aaded5c681e2d611a8c9bae6061e21d5b9bdf027
alozano20 <alvaro.lozanop29#gmail.com>
2023-10-03 12:01:38 +0200

    Ejercicios03/10

---
+++ PSP/EjerciciosPSP/calculadora.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h> 
+
+int main (int argc, char *argv[]){
+
+    if(argc < 3){
+        printf("El numero de argumentos que has pasado es menor que tres. Tienes que ingresa el parametro operacion, y dos numeros.");
+    }else{
+        const char *operacion = argv[1];
+        const char *num1_str = argv[2];
+        const char *num2_str = argv[3];
+        int resultado;
+        int num1 = atoi(num1_str);
+        int num2 = atoi(num2_str);
+
+        if(strcmp(operacion, "suma") == 0){
+            resultado = num1 + num2;
+            printf("El resultado es: %d\n",resultado);
+        }else if(strcmp(operacion, "resta") == 0){
+            resultado = num1 - num2;
+            printf("El resultado es: %d\n",resultado);
+        }else if(strcmp(operacion, "multiplicacion") == 0) {
+            resultado = num1 * num2;
+            printf("El resultado es: %d\n",resultado);
+        }else if(strcmp(operacion, "division") == 0){
+            if(num2 == 0){
+                printf("No se puede hacer una division entre cero.");
+            }else{
+                float resultadoDiv = (float)num1 / num2;
+                printf("El resultado es: %.2f\n",resultadoDiv);
+            }
+        }
+    }
+}
