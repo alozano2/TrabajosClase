4b87d84bb3fcaf6f4de0bc0f8500642ded3f6e5d
alozano20 <alvaro.lozanop29#gmail.com>
2023-11-06 11:19:59 +0100

    Ejercicio apagar Linux

---
+++ PSP/Java/EjercicioApagarLinux/shutdown.java
@@ -0,0 +1,53 @@
+package EjercicioApagarLinux;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class shutdown{
+    public static void main(String[] args) throws IOException {
+        int eleccion;
+        Scanner in = new Scanner(System.in);
+        Process apa;
+
+        String [] commandsApagar = {
+            "shutdown",
+            "-s"
+        };
+
+        String [] commandsReiniciar = {
+            "shutdown",
+            "-r"
+        };
+
+        String [] commandsHibernar = {
+            "shutdown",
+            "-h"
+        };
+
+        System.out.println("Elige una opcion: \n1. Apagar.\n2.Reiniciar.\n3.Hibernar");
+        eleccion = in.nextInt();
+
+        switch (eleccion) {
+            case 1:
+                System.out.println("Has decidido apagar el ordenador. Apagando......");
+                ProcessBuilder apagar = new ProcessBuilder(commandsApagar);
+                apa = apagar.start();
+                break;
+
+            case 2:
+                System.out.println("Has seleccionado reiniciar el ordenador. Reiniciando......");
+                ProcessBuilder reiniciar = new ProcessBuilder(commandsReiniciar);
+                apa = reiniciar.start();
+                break;
+            
+            case 3:
+                System.out.println("Has seleccionado hibernar. Hibernando.......");
+                ProcessBuilder hibernar = new ProcessBuilder(commandsHibernar);
+                apa = hibernar.start();
+                break;
+            default:
+                System.out.println("La opcion que has seleccionado no es correcta.");
+                break;
+        }
+    }
+}
