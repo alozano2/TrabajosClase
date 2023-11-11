bc1674e40048b51d70f8e1a9832fbd99542f85da
alozano20 <alvaro.lozanop29@gmail.com>
2023-11-11 12:37:17 +0100

    Ejercicio2Hilos terminado

---
+++ PSP/Java/EjerciciosHilos/Ejercicio2Hilos/Thread1al10.java
@@ -0,0 +1,55 @@
+package EjerciciosHilos.Ejercicio2Hilos;
+
+import java.util.ArrayList;
+
+public class Thread1al10 implements Runnable {
+    public static final int NUMERO_MAXIMO = 10;
+    private int numero;
+
+    public Thread1al10(int numero) {
+        this.numero = numero;
+    }
+    @Override
+    public void run() {
+        for(int i = 1; i < NUMERO_MAXIMO; i++){
+            for (int f = 0; f < NUMERO_MAXIMO; f++){
+                int resultado;
+                resultado = i * f;
+                System.out.println(i + " * " + f + " = " + resultado);
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+    
+        ArrayList<Thread> thread = new ArrayList<>();
+
+        for(int i = 1; i < NUMERO_MAXIMO; i++){
+            Thread1al10 b1 = new Thread1al10(i);
+            Thread tb1 = new Thread(b1);
+            tb1.setName("Tabla del " + i + ": ");
+            thread.add(tb1);
+        }
+        
+        for(int i = 1; i < NUMERO_MAXIMO; i++){
+            thread.get(i - 1).start();
+            System.out.println(thread.get(i - 1).getName());
+        }
+
+        for(int i = 1; i < NUMERO_MAXIMO; i++){
+            try{
+                thread.get(i - 1).join();
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /*Para hacer esto: Ejecuta el comando en la terminal, y vuelca su salida a un fichero. 
+        Utiliza las redirecciones de linux y el comando sort para verificar que has escrito
+        todas las tablas.
+        Habria que:
+        -Utilizar redirecciones Linux: java EjerciciosHilos.Ejercicio2Hilos.ModifArray > salida.txt
+        -Utilizar sort: sort salida.txt
+        Y así, una vez compilado el punto Java, conseguimos sacarlo a un fichero. */
+}
